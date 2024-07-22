e87066c39e2bdc51f29f1fc6ecb1266bab898b0c
Craig <craigtipple81@gmail.com>
2024-07-22 15:14:10 +0100

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+// import { AuthProvider } from "./components/AuthContext";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+import ErrorHandler from "./components/ErrorHandler";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Home = lazy(() => import("./components/Home"));
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col min-h-screen">
+      {/* <AuthProvider> */}
+      {/* <Header /> */}
+      <main className="main">
+        <Suspense
+          fallback={
+            <div className="flex justify-center">
+              <i className="fa-solid fa-spinner fa-spin"></i>
+            </div>
+          }>
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/error"
+              element={<ErrorHandler />}
+            />
+          </Routes>
+        </Suspense>
+      </main>
+      {/* <Footer /> */}
+      {/* </AuthProvider> */}
+    </div>
+  );
 }
-
-export default App

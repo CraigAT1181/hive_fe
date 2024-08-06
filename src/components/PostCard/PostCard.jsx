461df58cab48387428808d14d29ce20cad31aada
Craig <craigtipple81@gmail.com>
2024-08-06 19:07:04 +0100

    Added share icon on posts. Adjusted profile pic size and containing div.

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -3,17 +3,17 @@ import React from "react";
 export default function PostCard() {
   return (
     <div className="post-card-container">
-      <div className="min-w-24">
+      <div className="min-w-16 mx-2">
         <img
-          className="w-20 h-1/2 object-cover rounded"
+          className="w-16 h-16 object-cover rounded"
           src={"/profile-pic.jpg"}
           alt="profile pic"
         />
       </div>
       <div className="flex-grow">
-        <div className="flex justify-between font-semibold">
+        <div className="flex justify-between lg:justify-start font-semibold">
           <p className="">CraigT</p>
-          <p className="">@craigtipple</p>
+          <p className="lg:mx-4 font-thin">@craigtipple</p>
           <p className="">1h</p>
         </div>
         <div>
@@ -22,24 +22,28 @@ export default function PostCard() {
           a very long post, it should expand to comfortably display the entire
           thing.
         </div>
-        <div className="flex justify-between mt-2">
-          <p className="mb-0">
-            <i className="fa-solid fa-comment-dots"></i>
+        <div className="flex justify-between lg:justify-center mt-2">
+          <p className="mb-0 lg:mx-4">
+            <i className="fa-solid text-gray-400 fa-comment-dots"></i>
           </p>
-          <p className="mb-0">
-            <i className="fa-solid fa-share-nodes"></i>
+          <p className="mb-0 lg:mx-4">
+            <i className="fa-solid text-gray-400 fa-share-nodes"></i>
           </p>
-          <p className="mb-0">
-            <i className="fa-solid fa-thumbs-up"></i>
+          <p className="mb-0 lg:mx-4">
+            <i className="fa-solid text-gray-400 fa-thumbs-up"></i>
           </p>
-          <p className="mb-0">
-            <i className="fa-solid fa-bookmark"></i>
+          <p className="mb-0 lg:mx-4">
+            <i className="fa-solid text-gray-400 fa-bookmark"></i>
+          </p>
+          {"|"}
+          <p className="mb-0 lg:mx-4">
+            <i className="fa-solid text-gray-400 fa-share-from-square"></i>
           </p>
         </div>
         {/* LOGIC TO CHECK IF THERE'S A RETWEET. IF SO, RENDER DIV BELOW */}
-        <div className="border border-gray-500 p-2 mt-4">
+        {/* <div className="border border-gray-500 p-2 mt-4">
           A shortened version of the shared tweet would appear here.
-        </div>
+        </div> */}
       </div>
     </div>
   );
